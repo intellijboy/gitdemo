dee7ad76f62346911081a1ed3dfffee7e229f067
kakaluote444 <liuburu@outlook.com>
2017-03-20 15:46:44 +0800

    冲突测试之master提交了

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 ﻿
 public class Hello{
-	privat String brhName;
+	privat String brhName;//this is master note
 	public static void main(String[] args) {
 		System.out.println("你好Git先生！");
 		System.out.println("我叫刘卜铷，很高兴认识您~");
