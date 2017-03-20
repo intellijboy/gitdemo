0b524f09a4dcf2c016305a7a78f46a74234962a0
kakaluote444 <liuburu@outlook.com>
2017-03-20 15:51:18 +0800

    brh修改了注释

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 ﻿
 public class Hello{
-	privat String brhName;
+	privat String brhName;/*This is brh note*/
 	public static void main(String[] args) {
-		System.out.println("你好Git先生！");/*This is brh note*/
+		System.out.println("你好Git先生！");
 		System.out.println("我叫刘卜铷，很高兴认识您~");
 	}
 }
