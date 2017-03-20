6294de9e12d4553901fa960ab8842e99927169cc
kakaluote444 <liuburu@outlook.com>
2017-03-20 15:52:51 +0800

    把注释改成了大写

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 ﻿
 public class Hello{
-	privat String brhName;//this is master note
+	privat String brhName;//this is MASTER note
 	public static void main(String[] args) {
 		System.out.println("你好Git先生！");/*This is brh note*/
 		System.out.println("我叫刘卜铷，很高兴认识您~");
